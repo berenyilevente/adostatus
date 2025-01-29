67f38220315ea6312749dae125430b6ecc40d3b1
berenyilevente <berenyi.lev@gmail.com>
2025-01-29 14:26:43 +0100

    fix: login email header

--- src/components/Emails/LoginEmail/LoginEmail.tsx
+++ src/components/Emails/LoginEmail/LoginEmail.tsx
@@ -22,7 +22,7 @@ export const LoginEmail = ({ url, host }: { url: string; host: string }) => {
       <Preview>Log in with this magic link</Preview>
       <Body style={main}>
         <Container style={container}>
-          <Heading style={h1}>Log in to {host}</Heading>
+          <Heading style={h1}>Log in to {config.app.name}</Heading>
 
           <Link
             href={url}
@@ -84,9 +84,9 @@ const h1 = {
   color: "#333",
   fontFamily:
     "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-  fontSize: "24px",
+  fontSize: "16px",
   fontWeight: "bold",
-  margin: "40px 0",
+  margin: "32px 0",
   padding: "0",
 };
 
