e95bbbc1d5d1d31c8ebd6e6f9c187d95e639b2ee
Berenyi Levente <levente.berenyi@pastpay.com>
2026-02-09 13:29:55 +0100

    feat: update database url env variable

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,28 +1,28 @@
-import { assertEnvVar } from "@/lib/assertions/env";
+import { assertEnvVar } from '@/lib/assertions/env';
 
 // AUTH
-export const NEXTAUTH_SECRET = assertEnvVar("NEXTAUTH_SECRET", "");
-export const NEXTAUTH_URL = assertEnvVar("NEXTAUTH_URL", "");
+export const NEXTAUTH_SECRET = assertEnvVar('NEXTAUTH_SECRET', '');
+export const NEXTAUTH_URL = assertEnvVar('NEXTAUTH_URL', '');
 
 // RESEND
-export const EMAIL_SERVER = assertEnvVar("EMAIL_SERVER", "");
-export const RESEND_API_KEY = assertEnvVar("RESEND_API_KEY", "");
+export const EMAIL_SERVER = assertEnvVar('EMAIL_SERVER', '');
+export const RESEND_API_KEY = assertEnvVar('RESEND_API_KEY', '');
 
 // NODE_ENV
-export const NODE_ENV = assertEnvVar("NODE_ENV", "development");
+export const NODE_ENV = assertEnvVar('NODE_ENV', 'development');
 
 // BASE_URL
-export const BASE_URL = assertEnvVar("BASE_URL", "");
+export const BASE_URL = assertEnvVar('BASE_URL', '');
 
 // DATABASE
-export const POSTGRES_PRISMA_URL = assertEnvVar("POSTGRES_PRISMA_URL", "");
+export const PRISMA_DATABASE_URL = assertEnvVar('PRISMA_DATABASE_URL', '');
 
 // STRIPE
 export const STRIPE_PUBLISHABLE_KEY = assertEnvVar(
-  "STRIPE_PUBLISHABLE_KEY",
-  ""
+  'STRIPE_PUBLISHABLE_KEY',
+  ''
 );
-export const STRIPE_SECRET_KEY = assertEnvVar("STRIPE_SECRET_KEY", "");
-export const STRIPE_WEBHOOK_SECRET = assertEnvVar("STRIPE_WEBHOOK_SECRET", "");
-export const STRIPE_SUCCESS_URL = assertEnvVar("STRIPE_SUCCESS_URL", "");
-export const STRIPE_CANCEL_URL = assertEnvVar("STRIPE_CANCEL_URL", "");
+export const STRIPE_SECRET_KEY = assertEnvVar('STRIPE_SECRET_KEY', '');
+export const STRIPE_WEBHOOK_SECRET = assertEnvVar('STRIPE_WEBHOOK_SECRET', '');
+export const STRIPE_SUCCESS_URL = assertEnvVar('STRIPE_SUCCESS_URL', '');
+export const STRIPE_CANCEL_URL = assertEnvVar('STRIPE_CANCEL_URL', '');
